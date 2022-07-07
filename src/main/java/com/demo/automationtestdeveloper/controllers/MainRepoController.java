b060695d3fb0746f3ba6a1e1e75178a939f60d7c
hackedlings <pks9862728888@gmail.com>
2022-07-07 16:08:51 +0530

    Added code to clone main repo

--- src/main/java/com/demo/automationtestdeveloper/controllers/MainRepoController.java
+++ src/main/java/com/demo/automationtestdeveloper/controllers/MainRepoController.java
@@ -3,6 +3,8 @@ package com.demo.automationtestdeveloper.controllers;
 import com.demo.automationtestdeveloper.exceptions.CommandRunnerException;
 import com.demo.automationtestdeveloper.services.GithubService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +17,9 @@ public class MainRepoController {
     private GithubService githubService;
 
     @GetMapping("/reload-main-repo")
-    public void reloadMainRepo() throws CommandRunnerException {
+    public ResponseEntity<String> reloadMainRepo() throws CommandRunnerException {
         githubService.reCloneMainRepo();
+        return new ResponseEntity<>("OK", HttpStatus.OK);
     }
 
 }
