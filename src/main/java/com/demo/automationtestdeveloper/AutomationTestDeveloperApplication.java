d07b621d779bc414fb4a4281cc4b28809a3d0dd0
hackedlings <pks9862728888@gmail.com>
2022-07-07 14:19:08 +0530

    Initial commit

---
+++ src/main/java/com/demo/automationtestdeveloper/AutomationTestDeveloperApplication.java
@@ -0,0 +1,13 @@
+package com.demo.automationtestdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationTestDeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationTestDeveloperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/automationtestdeveloper/AutomationTestDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.automationtestdeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationTestDeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
