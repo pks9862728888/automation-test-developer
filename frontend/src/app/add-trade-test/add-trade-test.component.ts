876517301e3bf6b69a9e59bcd70bf5faa890f4d9
hackedlings <pks9862728888@gmail.com>
2022-07-10 00:22:32 +0530

    Added tag addition feature

--- frontend/src/app/add-trade-test/add-trade-test.component.ts
+++ frontend/src/app/add-trade-test/add-trade-test.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-add-trade-test',
@@ -14,11 +16,16 @@ export class AddTradeTestComponent implements OnInit {
   assetClass!: string;
   upi!: string;
 
+  // Feature file data
+  tagsList: Array<string> = [];
   steps: Array<string> = [];
   testName!: string;
-  testNameWithEventName!: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  // Forms
+  featureForm!: FormGroup;
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.jurisdiction = this.activatedRoute.snapshot.paramMap.get("jurisdiction") || "";
@@ -27,8 +34,7 @@ export class AddTradeTestComponent implements OnInit {
     this.upi = this.activatedRoute.snapshot.paramMap.get("upi") || "";
 
     // Predicted Test Name
-    this.testName = `${this.jurisdiction}_${this.sourceSystem}_${this.formatUpi(this.upi)}`;
-    this.testNameWithEventName = this.testName;
+    this.testName = `${this.jurisdiction}_${this.sourceSystem}_${this.formatUpi(this.upi)}`.toUpperCase();
 
     // Steps
     this.steps.push(`Feature: e2e test`);
@@ -40,7 +46,51 @@ export class AddTradeTestComponent implements OnInit {
     this.steps.push(`When Trade inputs are processed/published`);
     this.steps.push(`Then Trade should be reportable to jurisdiction ${this.jurisdiction}`);
     this.steps.push(`| Test Name |`);
-    this.steps.push(`| ${this.testNameWithEventName} |`);
+    this.steps.push(`${this.addTestName(this.testName)}`);
+
+    // Feature form
+    this.featureForm = this.fb.group({
+        event: ['', Validators.required],
+        testName: [this.testName, Validators.required],
+        tags: [this.getDefaultTags(), Validators.required]
+    });
+
+    this.featureForm.get("event")?.valueChanges.subscribe(newValue => {
+      // Update test name in form when event name changes
+      this.featureForm.get("testName")?.patchValue(`${this.testName}_${newValue}`.toUpperCase());
+    });
+
+    this.featureForm.get("testName")?.valueChanges.subscribe(newValue => {
+      newValue = newValue.toUpperCase();
+
+      // Update test name in feature file step definition
+      this.steps = this.steps.filter(t => !t.includes(this.testName));
+      this.steps.push(this.addTestName(newValue));
+
+      // Patch form value
+      this.featureForm.get("testName")?.patchValue(newValue);
+    });
+
+    this.featureForm.get("tags")?.valueChanges.pipe(
+        debounceTime(1000)
+    ).subscribe(() => this.updateTagsList());
+
+    // Tags
+    this.updateTagsList();
+  }
+
+  updateTagsList() {
+    let tags: Array<string> = [];
+    this.featureForm.get('tags')?.value.split(",").forEach((tag: string) => tags.push(`@${tag.trim()}`));
+    this.tagsList = tags;
+  }
+
+  getDefaultTags(): string {
+    return `E2E,${this.jurisdiction},${this.sourceSystem},${this.assetClass},${this.formatUpi(this.upi)}`;
+  }
+
+  addTestName(testName: string): string {
+    return `| ${testName} |`;
   }
 
   formatUpi(upi: string): string {
